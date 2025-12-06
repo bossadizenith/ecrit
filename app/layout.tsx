f32d401ec6401b85af1b3daad1fcb0f49b155e09
code-env <org.codeen@gmail.com>
2025-12-06 09:15:31 +0100

    feat: added the prose imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { Providers } from "@/contex";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "sonner";
-import "./globals.css";
+import "./styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
